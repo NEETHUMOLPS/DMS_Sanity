1a1579bc703273042ec920c586cf8401f15716e0
NeethumolPS <NeethumolPS@DM-LT8>
2024-04-12 16:59:05 +0530

    new

--- src/test/java/com/DMS/testCases/TC_013_TrainingPlan.java
+++ src/test/java/com/DMS/testCases/TC_013_TrainingPlan.java
@@ -5,25 +5,27 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.DMS.pageObjects.LoginPage;
 import com.DMS.pageObjects.TrainingPlan;
+import com.DMS.utilities.XLUtility;
 
 import Base.BaseClass;
 
 public class TC_013_TrainingPlan  extends BaseClass {
 
-	@Test(priority=1, description = "Add my training plan")
-	public void addTrainingPlan_MyTrainingPlan() throws IOException, InterruptedException
+	@Test(dataProvider = "DD1",priority=1, description = "Add my training plan")
+	public void addTrainingPlan_MyTrainingPlan(String email, String pwd, String sub, String PSD, String PED, String ASD, String AED, String TrainingMethod, String TrainedBy) throws IOException, InterruptedException
 	{
 		LoginPage lp=new LoginPage(driver);
-		lp.setEmail("neethumolp@datamatica.uk");
-		lp.setPassword("Ryan@1234");
+		lp.setEmail(email);
+		lp.setPassword(pwd);
 		lp.clickLogin();
 		TrainingPlan tp = new TrainingPlan(driver);
 		tp.clickOnTrainingPlan();
-		tp.clickOnAddTrainingPlan_MyTrainingPlan("Performance Testing", "07-04-2024", "07-04-2024", "07-04-2024", "07-04-2024", "Self", "Neethur R");
+		tp.clickOnAddTrainingPlan_MyTrainingPlan(sub, PSD, PED, ASD, AED, TrainingMethod, TrainedBy);
 		tp.addTrainingPlanAlert_MyTrainingPlan();	
 	}
 	
@@ -34,32 +36,95 @@ public class TC_013_TrainingPlan  extends BaseClass {
 		tp.export_MyTrainingPlan();
 	}
 	
-	@Test(priority=3, description = "Assign training plan")
-	public void addTrainingPlan_addTrainingPlan() throws IOException, InterruptedException
+	@Test(dataProvider = "DD2",priority=3, description = "Assign training plan")
+	public void addTrainingPlan_addTrainingPlan(String sub,String  PSD, String PED, String TrainingMethod, String TrainedBy) throws IOException, InterruptedException
 	{
 		TrainingPlan tp = new TrainingPlan(driver);
 		tp.clickOnAddTrainingPlan();
-		tp.clickOnAddTrainingPlan_AddTrainingPlan("Performance Testing", "07-04-2024", "07-04-2024", "Self", "Neethumol PS");
+		tp.clickOnAddTrainingPlan_AddTrainingPlan(sub, PSD, PED, TrainingMethod, TrainedBy);
 		tp.addTrainingPlanAlert_AddTrainingPlan();
 		LoginPage lp=new LoginPage(driver);
 		lp.profileIcon();
 		lp.logout();
 	}
 	
-	@Test(priority=4, description = "Sign the completed training plan")
-	public void sign_TrainingPlanForApproval() throws IOException, InterruptedException
+	@Test(dataProvider = "DD3",priority=4, description = "Sign the completed training plan")
+	public void sign_TrainingPlanForApproval(String email,String pwd,String status,String sub,String SignEmail, String SignPwd, String Remarks) throws IOException, InterruptedException
 	{
 		LoginPage lp=new LoginPage(driver);
-		lp.setEmail("neethur@yopmail.com");
-		lp.setPassword("Neethu@r1");
+		lp.setEmail(email);
+		lp.setPassword(pwd);
 		lp.clickLogin();
 		TrainingPlan tp = new TrainingPlan(driver);
 		tp.clickOnTrainingPlan();
 		tp.clickOnTrainingPlanForApproval();
-		tp.searchStatus2("Planned");
-		tp.sign1_TrainingPlanForApproval("Performance Testing");
-		tp.sign2_TrainingPlanForApproval("neethur@yopmail.com", "Neethu@r1", "Approval");
+		tp.searchStatus2(status);
+		tp.sign1_TrainingPlanForApproval(sub);
+		tp.sign2_TrainingPlanForApproval(SignEmail, SignPwd, Remarks);
 		tp.signALert_TrainingPlanForApproval();
+	}
+	
+	@DataProvider(name="DD1")
+	 String [][] getData1() throws IOException
+	{
+		//String path=System.getProperty("user.dir")+"/src/main/java/com/EIDSA/testData/EIDSA_Login_Negative.xlsx.xlsx";
+		String path = "C:\\Users\\NeethumolPS\\Desktop\\DMS\\TrainingPlan.xlsx";
+		int rownum=XLUtility.getRowCount(path, "MyTrainingPlan");
+	int colcount=XLUtility.getCellCount(path, "MyTrainingPlan", 1);
+	
+	String data[][]=new String[rownum][colcount];
+	for(int i=1;i<=rownum;i++)
+	{
+		for(int j=0;j<colcount;j++)
+		{
+			data[i-1][j]=XLUtility.getCellData(path, "MyTrainingPlan", i, j);
+		}
+	}
+	
+	return data;	
+
+	}
+	
+	@DataProvider(name="DD2")
+	 String [][] getData2() throws IOException
+	{
+		//String path=System.getProperty("user.dir")+"/src/main/java/com/EIDSA/testData/EIDSA_Login_Negative.xlsx.xlsx";
+		String path = "C:\\Users\\NeethumolPS\\Desktop\\DMS\\TrainingPlan.xlsx";
+		int rownum=XLUtility.getRowCount(path, "AddTrainingPlan");
+	int colcount=XLUtility.getCellCount(path, "AddTrainingPlan", 1);
+	
+	String data[][]=new String[rownum][colcount];
+	for(int i=1;i<=rownum;i++)
+	{
+		for(int j=0;j<colcount;j++)
+		{
+			data[i-1][j]=XLUtility.getCellData(path, "AddTrainingPlan", i, j);
+		}
+	}
+	
+	return data;	
+
+	}
+	
+	@DataProvider(name="DD3")
+	 String [][] getData3() throws IOException
+	{
+		//String path=System.getProperty("user.dir")+"/src/main/java/com/EIDSA/testData/EIDSA_Login_Negative.xlsx.xlsx";
+		String path = "C:\\Users\\NeethumolPS\\Desktop\\DMS\\TrainingPlan.xlsx";
+		int rownum=XLUtility.getRowCount(path, "Sign");
+	int colcount=XLUtility.getCellCount(path, "Sign", 1);
+	
+	String data[][]=new String[rownum][colcount];
+	for(int i=1;i<=rownum;i++)
+	{
+		for(int j=0;j<colcount;j++)
+		{
+			data[i-1][j]=XLUtility.getCellData(path, "Sign", i, j);
+		}
+	}
+	
+	return data;	
+
 	}
 	
 
--- src/test/java/com/DMS/testCases/TC_014_ConfigProcessTraining.java
+++ src/test/java/com/DMS/testCases/TC_014_ConfigProcessTraining.java
@@ -3,24 +3,47 @@ package com.DMS.testCases;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.DMS.pageObjects.ConfigProcessTraining;
+import com.DMS.utilities.XLUtility;
 
 import Base.BaseClassTest;
 
 public class TC_014_ConfigProcessTraining extends BaseClassTest {
 
 	
-	@Test(priority=1, description = "Assign SOP")
-	public void assignSOP() throws IOException, InterruptedException
+	@Test(dataProvider = "DD1",priority=1, description = "Assign SOP")
+	public void assignSOP(String TraineeName,String SOP, String Trainer, String StartDate, String EndDate) throws IOException, InterruptedException
 	{
 		ConfigProcessTraining cp = new ConfigProcessTraining(driver);
 		cp.clickOnConfigProcessTraining();
-		cp.searchByName("Neethumol PS");
-		cp.selUser("Neethumol PS");
-		cp.assignTraining("Problem Management", "Neethur R", "06-04-2024", "06-04-2024");
+		cp.searchByName(TraineeName);
+		cp.selUser(TraineeName);
+		cp.assignTraining(SOP, Trainer, StartDate, EndDate);
 		cp.invitationAlert();	
+	}
+	
+	@DataProvider(name="DD1")
+	 String [][] getData1() throws IOException
+	{
+		//String path=System.getProperty("user.dir")+"/src/main/java/com/EIDSA/testData/EIDSA_Login_Negative.xlsx.xlsx";
+		String path = "C:\\Users\\NeethumolPS\\Desktop\\DMS\\ConfigProcessTraining.xlsx";
+		int rownum=XLUtility.getRowCount(path, "Sheet1");
+	int colcount=XLUtility.getCellCount(path, "Sheet1", 1);
+	
+	String data[][]=new String[rownum][colcount];
+	for(int i=1;i<=rownum;i++)
+	{
+		for(int j=0;j<colcount;j++)
+		{
+			data[i-1][j]=XLUtility.getCellData(path, "Sheet1", i, j);
+		}
+	}
+	
+	return data;	
+
 	}
 	
 
